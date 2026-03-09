f90113eb25690b215c885bb1a5e73ded8927c862
Theodore Belo <96204247+theobelo25@users.noreply.github.com>
2026-03-09 13:58:33 -0400

    added global prisma-exception filter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule, PrismaExceptionFilter } from './prisma';
 import { UsersModule } from './domain/users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './domain/auth/auth.module';
 import { validateEnv, AppConfigModule } from './config';
 import { LoggerModule } from './logger/logger.module';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from './common';
 
 @Module({
@@ -29,6 +29,10 @@ import { ZodValidationPipe } from './common';
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: PrismaExceptionFilter,
     },
   ],
 })
--- apps/api/src/prisma/index.ts
+++ apps/api/src/prisma/index.ts
@@ -3,3 +3,4 @@ export * from './types/prisma.constants';
 export * from './types/db.type';
 export { PrismaClient, Prisma } from '@repo/database';
 export { prisma } from '@repo/database';
+export * from './prisma-exception.filter';
